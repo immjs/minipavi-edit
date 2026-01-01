00def4806fbc8a73bc51c20d95820ee5ad2ffcd8
Juliette Wang <39065949+immjs@users.noreply.github.com>
2026-01-01 22:42:01 +0000

    Update minitel-minipavi

--- dist/index.js
+++ dist/index.js
@@ -7,6 +7,11 @@ import { DuplexBridge } from 'ws-duplex-bridge';
 export default createMinipaviHandler(async (ws) => {
     const stream = new DuplexBridge(ws, { decodeStrings: false });
     const minitel = new Minitel(stream, { localEcho: false });
+    const interval = setInterval(() => {
+        if (!stream.writableEnded)
+            stream.write('\x00');
+    }, 10_000);
+    stream.on('end', () => clearInterval(interval));
     render(_jsx(App, {}), minitel);
 }, {
     host: '0.0.0.0',
